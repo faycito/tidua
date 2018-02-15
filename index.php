6f95dc7490c8923cdc844a7f154ed07fcdd38ce1
ismaelalexandermc <ismaelalexandermc@gmail.com>
2018-02-15 17:25:58 -0500

    encrypt

--- index.php
+++ index.php
@@ -40,33 +40,50 @@
             $text = '';
             $text2 = '';
             $text3 = '';
-  
+            $newDec = '';
+            // Inicia ciclo for para obtener el binario de cada letra y luego juntarlo (separandolo por un espacio) ↓↓
             for($i=0; strlen($variable)>$i; $i++){
+                // $bin tiene el valor binario de la cadena separando cada letra por espacios. 
                 $bin .= decbin(ord($variable[$i])).' ';
             }
+            // crea una nueva variable para utilizar $bin sin modificarlo. 
             $nuevavar = $bin;
-
+            // asigno variable que almacena el tamaño del string ↓↓
             $strSize = strlen($variable);
-
+            // Valor Binario del n° del tamaño del string ↑↑↓↓
             $newbin = decbin($strSize);
-
+            // ↓↓Obtengo la cantidad de numeros binarios de la variable $newbin↑ 
             $newbinleng = strlen($newbin);
             
-
+            // Concateno el valor binario del ($newbin) tamaño del string (linea 53)
             $nuevavar .=$newbin;
-
+            // hago un string sin espacios. este bin se convierte en array
             $bin3 = explode(' ', $nuevavar);
+
+            //For que convierte la variable Bin3 de array a cadena.
             for($i=0; count($bin3)>$i; $i++){
+                // creo la variable text2 para que el array sea convertido a un string. 
                 $text2 .= $bin3[$i];
             }
             $strwtfinbin = substr($text2, 0,-$newbinleng);
-
+            
+            for($i=0; strlen($text2)>$i; $i++){
+                $newDec .= bindec($text2[$i]);
+            }
+            $newOCt = decoct($strwtfinbin);
+            $ocDe = bin2hex($strwtfinbin);
+            $newHex = dechex($strwtfinbin);
+            $another = $newOCt.$ocDe.$newbin.$newHex;
+            $finalHash = '$Is%$'.$another.'$AL$';
             //desencriptar
             $bin2 = explode(' ', $bin);
             for($i=0; count($bin2)>$i; $i++){
                 $text .= chr(bindec($bin2[$i]));
             }
-            return $strwtfinbin;
+            // el string sin la cantidad binaria añadida
+
+            //salida final
+            return $finalHash;
         }
         $final = crypto();
         echo $final;
